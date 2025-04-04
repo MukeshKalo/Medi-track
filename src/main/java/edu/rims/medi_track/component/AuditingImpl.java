0e52f2ff668e30d348cc555628c4b8bca73296c4
Ashutosh Mohanty <153078795+HackerSpider46@users.noreply.github.com>
2025-04-04 21:17:28 +0530

    first commit

---
+++ src/main/java/edu/rims/medi_track/component/AuditingImpl.java
@@ -0,0 +1,21 @@
+package edu.rims.medi_track.component;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuditingImpl implements AuditorAware<String> {
+    @SuppressWarnings("null")
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.of("System");
+        }
+
+        return Optional.of(authentication.getName());
+    }
+}
